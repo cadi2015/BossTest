c65fe8fa66b73f995c38a664125fbeb64c4309e3
tyson2015 <cadi2015@outlook.com>
2016-05-27 17:12:12 +0800

    快速ui与dp的package信息

--- app/src/main/java/com/wp/bosstest/fragment/FragmentTool.java
+++ app/src/main/java/com/wp/bosstest/fragment/FragmentTool.java
@@ -1,5 +1,8 @@
 package com.wp.bosstest.fragment;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
@@ -16,6 +19,8 @@ import com.wp.bosstest.R;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by cadi on 2016/5/18.
@@ -25,8 +30,11 @@ public class FragmentTool extends Fragment {
     private View mRootView;
     private Button mBtnSwit;
     private TextView mTvShow;
+    private TextView mTvUiInfo;
+    private TextView mTvDpInfo;
     private Button mBtnAd;
     private String mRootPath;
+    private Context mContext;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,12 +46,41 @@ public class FragmentTool extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG, "onVeiwCreated(View view, Bundle savedInstanceState)");
         mRootPath = Environment.getExternalStorageDirectory().getPath();
+        mContext = getActivity();
         if (fileIsExists(mRootPath + File.separator + ".dlprovider" + File.separator + ".is_api_test")) {
             mTvShow.setText("测试环境");
         } else {
             mTvShow.setText("正式环境");
         }
+
+        StringBuilder uiSb = new StringBuilder();
+        StringBuilder dpSb = new StringBuilder();
+        PackageManager packageManager = mContext.getPackageManager();
+
+        try {
+            PackageInfo uiInfo = packageManager.getPackageInfo("com.android.providers.downloads.ui", PackageManager.GET_PERMISSIONS);
+            PackageInfo dpInfo = packageManager.getPackageInfo("com.android.providers.downloads", PackageManager.GET_PERMISSIONS);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            String uiLastTime = dateFormat.format(new Date(uiInfo.lastUpdateTime));
+            uiSb.append("ui版本号：" + uiInfo.versionName +"\n");
+            uiSb.append("包名: " + uiInfo.packageName + "\n");
+            uiSb.append("最后安装时间:" + uiLastTime + "\n");
+            mTvUiInfo.setText(uiSb.toString());
+
+            String dpLastTime = dateFormat.format(new Date(dpInfo.lastUpdateTime));
+
+            dpSb.append("dp版本号：" + dpInfo.versionName + "\n");
+            dpSb.append("包名：" + dpInfo.packageName + "\n");
+            dpSb.append("最后安装时间:" + dpLastTime + "\n");
+            mTvDpInfo.setText(dpSb.toString());
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            Log.d(TAG, "找不到指定包的info信息");
+        }
+
+
     }
 
     private boolean fileIsExists(String path) {
@@ -59,6 +96,8 @@ public class FragmentTool extends Fragment {
         mBtnSwit = (Button) mRootView.findViewById(R.id.tool_btn_switch);
         mTvShow = (TextView) mRootView.findViewById(R.id.tool_tv_current_show);
         mBtnAd = (Button) mRootView.findViewById(R.id.tool_btn_ad);
+        mTvUiInfo = (TextView)mRootView.findViewById(R.id.tool_tv_ui_info);
+        mTvDpInfo = (TextView)mRootView.findViewById(R.id.tool_tv_dp_info);
         View.OnClickListener myClick = new MyBtnClick();
         mBtnSwit.setOnClickListener(myClick);
         mBtnAd.setOnClickListener(myClick);
@@ -85,7 +124,7 @@ public class FragmentTool extends Fragment {
                             }
                         }
                         if (haveTestApiFile) {
-                            Toast.makeText(getActivity(), "切换正式环境成功", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(mContext, "切换正式环境成功", Toast.LENGTH_SHORT).show();
                             apiTestFile.delete();
                             mTvShow.setText("正式环境");
                         } else {
@@ -94,7 +133,7 @@ public class FragmentTool extends Fragment {
                                 try {
                                     is_api_test.createNewFile();
                                     mTvShow.setText("测试环境");
-                                    Toast.makeText(getActivity(), "切换测试环境成功", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(mContext, "切换测试环境成功", Toast.LENGTH_SHORT).show();
                                 } catch (IOException e) {
                                     e.printStackTrace();
                                 }
@@ -102,7 +141,7 @@ public class FragmentTool extends Fragment {
                         }
 
                     } else {
-                        Toast.makeText(getActivity(), ".dlprovider目录不存在", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(mContext, ".dlprovider目录不存在", Toast.LENGTH_SHORT).show();
                     }
                     break;
                 case R.id.tool_btn_ad:
@@ -112,12 +151,12 @@ public class FragmentTool extends Fragment {
                         if(!ad_show.exists()) {
                             try {
                                 ad_show.createNewFile();
-                                Toast.makeText(getActivity(), "创建成功", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(mContext, "创建成功", Toast.LENGTH_SHORT).show();
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
                         } else {
-                            Toast.makeText(getActivity(), ".ad_show文件已经创建", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(mContext, ".ad_show文件已经创建", Toast.LENGTH_SHORT).show();
                         }
                     }
                     break;
